4cbe6daa72acb748130516deff1774d77b0a5e76
Mason Newell <mason.newell13@gmail.com>
2022-01-02 16:20:22 -0800

    blog-routes now read from mysql, views adjusted

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -1,21 +1,29 @@
 const router = require("express").Router();
 const Blog = require("../models/Blog");
 
-const blogPost = [
-  {
-    test: "tester",
-  },
-];
-
 // Get All Blog Posts
 router.get("/", async (req, res) => {
-  res.render("all");
+  const blogData = await Blog.findAll().catch((err) => {
+    res.json(err);
+  });
+  const blogPost = blogData.map((blog) => blog.get({ plain: true }));
+  res.render("all", { blogPost });
 });
 
 // Get One Blog Post
-router.get("/blog/:num", async (req, res) => {
-  // name of view, rendering logic
-  return res.render("blog", blogPost[req.params.num - 1]);
+router.get("/blog/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id);
+    if (!blogData) {
+      res.status(404).json({ message: "No blogpost with this id!" });
+      return;
+    }
+    // serialize data
+    const blog = blogData.get({ plain: true });
+    res.render("blog", blog);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
