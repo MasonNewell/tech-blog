dbae94d08327d22c88de162c5a827605daa01cf7
Mason Newell <mason.newell13@gmail.com>
2022-01-29 15:11:13 -0800

    renamed file

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ const sess = {
 //  Middleware
 app.use(session(sess));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(require("./controllers/index"));
 
 // Start server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
